2a1e06b9019234fb64411beac9a6be53f1d98c3f
Ari Azo <ari_azo@hotmail.com>
2024-06-30 14:17:20 +0200

    Projektdateien hinzufügen.

---
+++ Ueb11a_StrassenverkehrsamtTest/UnitTestPerson.cs
@@ -0,0 +1,16 @@
+using Ueb11a_Strassenverkehrsamt;
+
+namespace Ueb11a_StrassenverkehrsamtTest
+{
+    [TestClass]
+    public class UnitTestPerson
+    {
+        [TestMethod]
+        public void TestMethode_Konstruktor()
+        {
+            Person p = new Person();
+
+            Assert.IsTrue(true);
+        }
+    }
+}
